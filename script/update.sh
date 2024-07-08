550650808df87f1152d4eb9acf4003025bda2aa9
Yelshat Duskaliyev <ura2178@gmail.com>
2024-07-08 11:57:43 +0500

    fix: chown build dir to nginx

--- script/update.sh
+++ script/update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# pull, build, and package the grype database, and set the owner to nginx
+
 /usr/local/bin/grype-db -c /srv/grypedb.yaml pull && \
 /usr/local/bin/grype-db -c /srv/grypedb.yaml build && \
-/usr/local/bin/grype-db -c /srv/grypedb.yaml package || exit 1
+/usr/local/bin/grype-db -c /srv/grypedb.yaml package && \
+chown -R nginx:nginx /build || exit 1
